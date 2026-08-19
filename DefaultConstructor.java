0689192aa6d12d527dd5e113a4d2391cb7c0b54a
VISHWA <vishwaakash959@gmail.com>
2026-08-19 13:58:07 +0530

    program added

---
+++ DefaultConstructor.java
@@ -0,0 +1,15 @@
+
+class Room {
+
+    double length;
+    double breadth;
+}
+
+public class DefaultConstructor {
+
+    public static void main(String[] args) {
+        Room I = new Room();
+        System.out.println("Length: " + I.length);
+        System.out.println("Breadth: " + I.breadth);
+    }
+}
