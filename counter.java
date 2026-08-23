5674c63a5c166f993b21350e008ad14d494d8cda
VISHWA <vishwaakash959@gmail.com>
2026-08-23 22:12:10 +0530

    code added

--- counter.java
+++ counter.java
@@ -1,15 +1,16 @@
-class counter
-{
-    int count=0;
-    counter()
-    {
+
+class counter {
+
+    int count = 0;
+
+    counter() {
         count++;
         System.out.println(count);
     }
-    public static void main(String[] args)
-    {
-        counter c1=new counter();
-        counter c2=new counter();
-        counter c3=new counter();
+
+    public static void main(String[] args) {
+        counter c1 = new counter();
+        counter c2 = new counter();
+        counter c3 = new counter();
     }
 }
