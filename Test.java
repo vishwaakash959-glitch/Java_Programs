e2abce03d2939a83f3be6081bcb5ffc11ee2c9e4
VISHWA <vishwaakash959@gmail.com>
2026-09-04 17:32:04 +0530

    Add interfaces Printable and Showable with Document class implementation

--- Test.java
+++ Test.java
@@ -1,10 +1,35 @@
-import java.util.Scanner;
+// 1. Define Interfaces
 
+interface Printable {
+
+    void print();
+}
+
+interface Showable {
+
+    void show();
+}
+
+// 2. Implement the Interfaces in a Class
+class Document implements Printable, Showable {
+
+    @Override
+    public void print() {
+        System.out.println("Printing document...");
+    }
+
+    @Override
+    public void show() {
+        System.out.println("Showing document...");
+    }
+}
+
+// 3. Main Public Class (File must be named Test.java)
 public class Test {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        System.out.println("You entered: " + n);
+        Document d = new Document();
+        d.print();
+        d.show();
     }
 }
