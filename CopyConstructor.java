40b246b1e37702b956ede80c5e74482feb12be92
VISHWA A <vishwaakash959@gmail.com>
2026-09-08 20:05:57 +0530

    Refactor point class and add copy constructor

--- CopyConstructor.java
+++ CopyConstructor.java
@@ -1,21 +1,15 @@
-
 class point {
-
     int x, y;
-
     point(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
     point(point p) {
         this.x = p.x;
         this.y = p.y;
     }
 }
-
 public class CopyConstructor {
-
     public static void main(String[] args) {
         point p1 = new point(10, 20);
         point p2 = new point(p1);
