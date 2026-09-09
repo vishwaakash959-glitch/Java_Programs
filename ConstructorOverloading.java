7fe32dc7bb7fd683db9c2df1f2b2efeb236e4663
VISHWA <vishwaakash959@gmail.com>
2026-09-09 13:19:59 +0530

    Implement main method to demonstrate Box class functionality and volume calculation

--- ConstructorOverloading.java
+++ ConstructorOverloading.java
@@ -23,3 +23,16 @@ class Box {
         return length * breadth * height;
     }
 }
+
+public class ConstructorOverloading {
+
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        Box box2 = new Box(5);
+        Box box3 = new Box(2, 3, 4);
+
+        System.out.println("Volume of box1: " + box1.volume());
+        System.out.println("Volume of box2: " + box2.volume());
+        System.out.println("Volume of box3: " + box3.volume());
+    }
+}
