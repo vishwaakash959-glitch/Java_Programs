0283b3b0065dc994aaef467b11f964cade80fea1
VISHWA A <vishwaakash959@gmail.com>
2026-09-13 19:44:52 +0530

    Implement constructor overloading in Box class
    Demonstrate constructor overloading in Java with a Box class that supports default, single-parameter, and multi-parameter constructors.

--- ConstructorOverloading.java
+++ ConstructorOverloading.java
@@ -1,36 +1,28 @@
-
+//To demonstrate the use of constructor overloading concept in java
 class Box {
-
     double length, breadth, height;
-
     Box() {
         length = 10;
         breadth = 10;
         height = 10;
     }
-
     Box(double side) {
         length = breadth = height = side;
     }
-
     Box(double l, double b, double h) {
         length = l;
         breadth = b;
         height = h;
     }
-
     double volume() {
         return length * breadth * height;
     }
 }
-
 public class ConstructorOverloading {
-
     public static void main(String[] args) {
         Box box1 = new Box();
         Box box2 = new Box(5);
         Box box3 = new Box(2, 3, 4);
-
         System.out.println("Volume of box1: " + box1.volume());
         System.out.println("Volume of box2: " + box2.volume());
         System.out.println("Volume of box3: " + box3.volume());
